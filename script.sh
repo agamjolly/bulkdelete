da78d652c7c60f119970a5ab53b7ea5d5b211900
Agam Jolly <me@agamjolly.com>
2019-06-13 19:34:16 +0530

    First upload

---
+++ script.sh
@@ -0,0 +1,1 @@
+http https://api.github.com/user/repos "Authorization:token {$TOKEN}" per_page==100 type==owner | jq '.[].full_name' | xargs -I '{}' http DELETE https://api.github.com/repos/'{}' "Authorization:token {$TOKEN}}" 
